1438dae3e0dcc63ec1c77f84fc25c68cd7c27a70
ImDanosaur <sanchezz611@gmail.com>
2019-10-03 14:23:30 -0600

    Router installed

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import '../SideDrawer/DrawerToggleButton';
 import './Toolbar.css';
 import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
+import App from '../../App';
+import AboutMe from '../AboutMe/AboutMe';
 
 
 
 const Toolbar = props => (
+  <Router>
   <header className="toolbar">
     <nav className="toolbar-nav">
       <div className="toolbar-toggle-button">
@@ -16,7 +20,7 @@ const Toolbar = props => (
       <div className="spacer" />
       <div className="toolbar-nav-items">
         <ul>
-          <li><a href="/">About Me</a></li>
+          <li><Link to="/about">About Me</Link></li>
           <li><a href="/">Projects</a></li>
           <li><a href="/">Resume</a></li>
           <li><a href="/">LinkedIn </a></li>
@@ -24,6 +28,8 @@ const Toolbar = props => (
       </div>
     </nav>
   </header>
+  <Route path='/about' component={AboutMe} />
+  </Router>
 );
 
 export default Toolbar;
