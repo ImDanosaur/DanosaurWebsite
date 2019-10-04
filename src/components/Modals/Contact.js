8db248ce51af54063a043d95abdaf7bada65e8f0
Josh Levy <joshaaronlevy@gmail.com>
2019-10-04 01:48:02 -0600

    Updated app with global styling

--- src/components/Modals/Contact.js
+++ src/components/Modals/Contact.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './Contact.css';
+import '../../App.css';
 
 function Contact() {
   return(
-    <div className="about-page">
-      <div className="about-me">
+    <div className="app-container">
+      <div className="app-content">
         <img className="about-image" src="https://i.imgur.com/q3ehgGf.png" alt="Danosaur" />
         <h1>Contact Me!</h1>
       </div>
